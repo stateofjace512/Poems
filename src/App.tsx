4f3c86b3c484dc754b0bd40a07ac1611e35d3361
stateofjace512 <jakeryanrobison@icloud.com>
2025-08-21 02:29:52 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ArticlesHomePage from './pages/ArticlesHomePage';
 import NotFound from './pages/NotFound';
-
 // Import all song components
 import TSTT from '/src/pages/page/TSTT';
 import TNG from '/src/pages/page/TNG';
@@ -12,8 +11,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ArticlesHomePage />} />
-
-
+        {/* Add the missing routes */}
+        <Route path="/page/tng/" element={<TNG />} />
+        <Route path="/page/tstt/" element={<TSTT />} />
         {/* Catch-all for unmatched routes */}
         <Route path="*" element={<NotFound />} />
       </Routes>

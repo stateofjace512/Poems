f67af44421ea088c1a4995011be0e8029bee6be0
stateofjace512 <jakeryanrobison@icloud.com>
2025-08-23 18:38:36 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ArticlesHomePage from './pages/ArticlesHomePage';
 import NotFound from './pages/NotFound';
 import TNGFixer from '../components/TNGFixer.jsx';
+
 // Import all song components
 import TSTT from './pages/page/TSTT.jsx';
 import TNG from './pages/page/TNG.jsx';
@@ -10,12 +11,11 @@ import TNG from './pages/page/TNG.jsx';
 function App() {
   return (
     <BrowserRouter>
+      <TNGFixer />
       <Routes>
         <Route path="/" element={<ArticlesHomePage />} />
-        {/* Add the missing routes */}
         <Route path="/page/tng/" element={<TNG />} />
         <Route path="/page/tstt/" element={<TSTT />} />
-        {/* Catch-all for unmatched routes */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

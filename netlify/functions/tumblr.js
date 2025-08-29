2b30e5786491db7f0f81220139c4a79ecac1f60d
stateofjace512 <jakeryanrobison@icloud.com>
2025-08-29 17:19:23 -0500

    Create tumblr.js

---
+++ netlify/functions/tumblr.js
@@ -0,0 +1,19 @@
+import fetch from "node-fetch";
+
+export async function handler(event) {
+  const apiKey = process.env.TUMBLR_API_KEY;
+  const blog = "jrosales19"; // your blog
+  const url = `https://api.tumblr.com/v2/blog/${blog}/posts/text?api_key=${apiKey}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return {
+      statusCode: 200,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+}

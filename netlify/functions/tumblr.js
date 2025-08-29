fec6998bab615d3ab680def755d366dbe4138e3c
stateofjace512 <jakeryanrobison@icloud.com>
2025-08-29 17:28:48 -0500

    Update tumblr.js

--- netlify/functions/tumblr.js
+++ netlify/functions/tumblr.js
@@ -1,19 +1,26 @@
-import fetch from "node-fetch";
+// netlify/functions/tumblr.js
 
 export async function handler(event) {
-  const apiKey = 'zfgzSTPVBDa86jBZtWif8xBqXduItRE4pX3l1tWAtBT8k12qZG';
-  const blog = "jrosales19"; // your blog
+  const apiKey = process.env.TUMBLR_API_KEY; 
+  const blog = "jrosales19.tumblr.com";
   const url = `https://api.tumblr.com/v2/blog/${blog}/posts/text?api_key=${apiKey}`;
 
   try {
     const res = await fetch(url);
     const data = await res.json();
+
     return {
       statusCode: 200,
-      headers: { "Access-Control-Allow-Origin": "*" },
+      headers: {
+        "Access-Control-Allow-Origin": "*", 
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(data),
     };
   } catch (err) {
-    return { statusCode: 500, body: err.toString() };
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
   }
 }

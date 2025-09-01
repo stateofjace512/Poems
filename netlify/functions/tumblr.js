4a40e479154b361f60cea1ff183ea0d54cfb7671
stateofjace512 <jakeryanrobison@icloud.com>
2025-09-01 00:38:41 -0500

    Update tumblr.js

--- netlify/functions/tumblr.js
+++ netlify/functions/tumblr.js
@@ -1,19 +1,39 @@
-export async function handler(event) {
-  const apiKey = process.env.TUMBLR_API_KEY;
-  const blog = "jrosales19.tumblr.com";
+import OAuth from "oauth-1.0a";
+import crypto from "crypto";
 
-  const url = `https://api.tumblr.com/v2/blog/${blog}/posts?api_key=${apiKey}&npf=true&reblog_info=true&notes_info=true&limit=50`;
+export async function handler(event) {
+  const consumerKey = process.env.TUMBLR_API_KEY;
+  const consumerSecret = process.env.TUMBLR_SECRET_KEY;
+  const accessToken = process.env.TUMBLR_ACCESS_TOKEN;
+  const accessTokenSecret = process.env.TUMBLR_ACCESS_KEY_SECRET;
+
+  const blog = "jrosales19.tumblr.com";
+  const url = `https://api.tumblr.com/v2/blog/${blog}/posts`;
+
+  // Init OAuth
+  const oauth = new OAuth({
+    consumer: { key: consumerKey, secret: consumerSecret },
+    signature_method: "HMAC-SHA1",
+    hash_function(base_string, key) {
+      return crypto.createHmac("sha1", key).update(base_string).digest("base64");
+    },
+  });
+
+  // Sign request
+  const requestData = { url, method: "GET" };
+  const headers = oauth.toHeader(
+    oauth.authorize(requestData, {
+      key: accessToken,
+      secret: accessTokenSecret,
+    })
+  );
 
   try {
-    const res = await fetch(url);
+    const res = await fetch(url, { method: "GET", headers });
     if (!res.ok) {
       throw new Error(`Tumblr API error: ${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-
-    const filteredPosts = data?.response?.posts?.filter(
-      (post) => !post.reblogged_from_id && !post.reblogged_root_id
-    ) || [];
 
     return {
       statusCode: 200,
@@ -21,17 +41,7 @@ export async function handler(event) {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(
-        {
-          ...data,
-          response: {
-            ...data.response,
-            posts: filteredPosts,
-          },
-        },
-        null,
-        2
-      ),
+      body: JSON.stringify(data, null, 2),
     };
   } catch (err) {
     return {
